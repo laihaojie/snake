76b4ff91c79fd21bc299349f95d0127c2ed14ea1
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-07-25 15:49:22 +0800

    chore: update

--- src/icp.ts
+++ src/icp.ts
@@ -3,7 +3,7 @@ const host_name = location.hostname
 if (host_name.includes('laihaojie.top')) {
   icp.textContent = '沪ICP备2022020790号-1'
 } else if (host_name.includes('laihaojie.com')) {
-  icp.textContent = '222222'
+  icp.textContent = '沪ICP备2022020790号-2'
 } else {
   icp.textContent = '沪ICP备16007007号-16'
 }
